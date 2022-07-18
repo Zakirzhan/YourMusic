7580d1b6bd894c47e36fb4e083e00634926f115e
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-07-18 16:40:01 +0300

    feat: add search controller

---
+++ YourMusic/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  YourMusic
+//
+//  Created by Kirill Sytkov on 18.07.2022.
+//
+
+import Foundation
---
+++ YourMusic/Model/TrackModel.swift
@@ -0,0 +1,8 @@
+//
+//  TrackModel.swift
+//  YourMusic
+//
+//  Created by Kirill Sytkov on 18.07.2022.
+//
+
+import Foundation
